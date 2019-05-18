eef4cf4ab0cc9af8ba00604f652a5cc48e8185c4
learn <burmesterelyse@gmail.com>
2019-05-18 10:06:19 -0700

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+currentUser = "Elyse Burmester"
+welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+
+shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"
